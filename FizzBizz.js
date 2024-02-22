c9b0cdcac5ad4c06a096010af29e52fb6e64b954
jonnie254 <johnkamanu254@proton.me>
2024-02-22 15:29:32 +0300

    question FizzBuzz

--- FizzBizz.js
+++ FizzBizz.js
@@ -1,14 +1,12 @@
 /*Question 1 - FizzBizz */
-function fizzBizz(n) {
-  for (let i = 1; i <= n; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log("FizzBizz");
-    } else if (i % 3 === 0) {
-      console.log("Fizz");
-    } else if (i % 5 === 0) {
-      console.log("Bizz");
-    } else {
-      console.log(i);
-    }
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
   }
 }
