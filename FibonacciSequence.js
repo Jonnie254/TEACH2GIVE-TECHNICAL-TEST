1c384946b79a477c2add77dd649b63c3048c284b
jonnie254 <johnkamanu254@proton.me>
2024-02-22 15:32:12 +0300

    QUESTION 2 - FIBONACCI SEQUENCE

---
+++ FibonacciSequence.js
@@ -0,0 +1,13 @@
+/* Question 2 - Fibonacci Sequence */
+
+function fibonacci() {
+  let fibArray = [0, 1];
+  //loop through the array and add the last two numbers to get the next number
+  for (let i = 2; fibArray[i - 1] + fibArray[i - 2] <= 100; i++) {
+    fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
+  }
+  console.log("Question 2 - Fibonacci Sequence");
+  console.log(fibArray.join(", "));
+}
+// Call the function
+fibonacci();
